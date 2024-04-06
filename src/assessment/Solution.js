308f19c0f68136fd51fe4328370628e50fe89960
smartmohan20 <dev.smartmohan@gmail.com~>
2024-04-06 13:47:19 +0530

    Removed unnecessory classes

--- src/assessment/Solution.js
+++ src/assessment/Solution.js
@@ -2,16 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 const Solution = () => {
   const [error, setError] = useState(null);
-  
+
   if (error) {
     return <div>Error: {error}</div>;
   }
   
   return (
-    <div className="Solution">
-      <header className="Solution-header">
-        <h3>Solution</h3>
-      </header>
+    <div>
+      <h3>Solution</h3>
     </div>
   );
 }
