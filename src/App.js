be307574b2f3a70fcff1a59e825d69877093adb9
smartmohan20 <dev.smartmohan@gmail.com~>
2024-04-06 13:09:33 +0530

    Added solution component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Solution from './assessment/Solution';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Spearmint Technologies Assessment</h1>
+        <Solution />
       </header>
     </div>
   );
---
+++ src/assessment/Solution.js
@@ -0,0 +1,11 @@
+function Solution() {
+  return (
+    <div className="Solution">
+      <header className="Solution-header">
+        <h3>Solution</h3>
+      </header>
+    </div>
+  );
+}
+
+export default Solution;
